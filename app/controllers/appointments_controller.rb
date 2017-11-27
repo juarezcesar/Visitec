621e89ad9e5ea8786eb17c274cdacb50d630a7f8
Juarez <juarez@cesar.etc.br>
2017-11-27 08:50:15 -0200

    Refactoring Google API request code, add secretes.yml to gitignore

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -7,18 +7,17 @@ class AppointmentsController < ApplicationController
 
   def calculate_distance
 
-    google_key = 'AIzaSyCyiJTAsYyAGaJ5LV73kIiLbKqWEIWZw7c'
+    google_key = Rails.application.secrets.google_api_map_matrix
+    google_url = 'https://maps.googleapis.com/maps/api/distancematrix/json?'
+    google_params = {origins: 'Curitiba', destinations: 'Mandirituba', key: google_key}
 
-    url = URI.parse "https://maps.googleapis.com/maps/api/distancematrix/json?origins=Curitiba&destinations=Mandirituba&key=AIzaSyCyiJTAsYyAGaJ5LV73kIiLbKqWEIWZw7c"
+    url = URI.parse google_url + google_params.to_query
 
     req = Net::HTTP::Get.new(url)
-
 
     res = Net::HTTP.start(url.host, url.port,:use_ssl => true, :verify_mode => OpenSSL::SSL::VERIFY_NONE) { |http|
       http.request(req)
     }
-
-
 
     @distance = res.body
 
